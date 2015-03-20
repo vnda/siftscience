dc6a4d15e8f785a9f48eab1d5b5948883c1037b1
Francisco DC <fdc@razaoinfo.com.br>
2015-03-19 22:44:57 -0300

    make it possible create shop without create sift science account

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -16,17 +16,19 @@ class ShopsController < ApplicationController
     @shop = Shop.new(shop_params)
     if @shop.save
 
-      response = Excon.post("https://api3.siftscience.com/v3/partners/#{ENV["PARTNER_ID"]}/accounts",
-        :body => URI.encode_www_form(
-          :site_url => @shop.vnda_api_host,
-          :site_email => "#{params[:subdomain]}@vnda.com.br",
-          :analyst_email => "#{params[:subdomain]}@vnda.com.br",
-          :password => "#{params[:subdomain]}1101"
-        ),
-        :headers => {'Authorization' => "Basic #{ENV["PARTNER_KEY"]}"})
-      account = MultiJson.load(response.body)
-      @shop.sift_api_key = account[ Rails.env.production? ? "production" : "sandbox" ]["api_keys"].first["key"]
-      @shop.save
+      if params[:subdomain].present?
+        response = Excon.post("https://api3.siftscience.com/v3/partners/#{ENV["PARTNER_ID"]}/accounts",
+          :body => URI.encode_www_form(
+            :site_url => @shop.vnda_api_host,
+            :site_email => "#{params[:subdomain]}@vnda.com.br",
+            :analyst_email => "#{params[:subdomain]}@vnda.com.br",
+            :password => "#{params[:subdomain]}1101"
+          ),
+          :headers => {'Authorization' => "Basic #{ENV["PARTNER_KEY"]}"})
+        account = MultiJson.load(response.body)
+        @shop.sift_api_key = account[ Rails.env.production? ? "production" : "sandbox" ]["api_keys"].first["key"]
+        @shop.save
+      end
 
       redirect_to edit_shop_path(@shop), notice: "Loja cadastrada com sucesso #{account}"
     else
